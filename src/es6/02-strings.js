ebc5a256a22df56949c6162d5b95f40100aa92d3
brayanrodallega <thebrayan1a1@outlook.com>
2023-02-05 17:46:23 -0500

    strings

---
+++ src/es6/02-strings.js
@@ -0,0 +1,18 @@
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world;
+console.log(epicPhrase);
+
+// Template literals
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
+
+// Multi-line strings
+let lorem = 'esto es una frase epica \n' + 'otra frase epica que necesitamos';
+
+// With template literals
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica`;
+
+console.log(lorem);
+console.log(lorem2);
