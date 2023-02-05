aa43785567ea7f6e9a5f0b326d607acc5953d348
brayanrodallega <thebrayan1a1@outlook.com>
2023-02-05 17:27:21 -0500

    arrow function

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,11 @@
+function square(x) {
+  return x * x;
+}
+
+// Arrow function
+const squareArrow = (x) => {
+    return x * x;
+};
+
+// Arrow function expression syntax
+const squareArrow2 = x => x * x;
