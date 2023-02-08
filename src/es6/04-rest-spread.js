addf13de30662a1ee3bd7712f08be16f14a43700
brayanrodallega <thebrayan1a1@outlook.com>
2023-02-07 23:55:43 -0500

    spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -13,3 +13,22 @@ let person = {
 
 let { name, age, country } = person;
 console.log(name, age, country);
+
+
+// * Spread operator
+
+let person = { name: 'Oscar', age: 32};
+let country = 'MX'
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+// * Rest operator
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
