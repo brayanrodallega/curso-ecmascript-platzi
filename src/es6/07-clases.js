ee98bff0a55e7cb5bdd06dfcd582e7f26b63a7b5
brayanrodallega <thebrayan1a1@outlook.com>
2023-02-20 14:39:42 -0500

    class

---
+++ src/es6/07-clases.js
@@ -0,0 +1,14 @@
+// declarndo una clase
+class Persona {}
+// instanciando una clase
+// const persona = new Persona();
+
+class User {
+    // metodos
+    greeting() {
+        return 'Hello World';
+    }
+};
+
+const user = new User();
+console.log(user.greeting());
